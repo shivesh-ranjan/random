b7c9310599b77bb2e631b2f1d1f71cde9b2199d8
Shivesh Ranjan <86239697+shivesh-ranjan@users.noreply.github.com>
2025-02-04 11:02:45 +0530

    Update master.sh

--- k8s/scripts/master.sh
+++ k8s/scripts/master.sh
@@ -38,6 +38,13 @@ mkdir -p "$HOME"/.kube
 sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
 sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 
-# Install Claico Network Plugin Network 
+# Install Cilium Network Plugin Network 
+CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
+CLI_ARCH=amd64
+if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
+curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
+rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
 
-kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+cilium install --version 1.16.6
